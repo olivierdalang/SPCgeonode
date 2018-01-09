d8cfd5aae171a8491e4ec274a1a7807813a6353b
Olivier Dalang <olivier.dalang@gmail.com>
2018-01-09 15:40:01 +1300

    celery works

--- django/spcnode/settings.py
+++ django/spcnode/settings.py
@@ -3,7 +3,6 @@ from geonode.settings import *
 from datetime import timedelta
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Can be removed after geonode>=2.7.x as it will be like this in main settings
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
@@ -21,21 +20,18 @@ INSTALLED_APPS = SPCNODE_APPS + INSTALLED_APPS
 
 
 
-OFFLINE_OSM_UPDATE_INTERVAL = 5 # TODO : remove this
+OFFLINE_OSM_UPDATE_INTERVAL = 1 # TODO : remove this
 OFFLINE_OSM_DATA_DIR = '/spcnode-media/urlretrieve/' # TODO : remove this ?
+OFFLINE_OSM_UPDATE_AFTER_MIGRATE = False
 
 CELERY_IMPORTS = CELERY_IMPORTS + ('geonode_offlineosm.tasks',)
-CELERYBEAT_SCHEDULE = {
-    'update_offline_osm': {
-        'task': 'geonode_offlineosm.tasks.update_offline_osm',
-        'schedule': timedelta(minutes=OFFLINE_OSM_UPDATE_INTERVAL),
-        # 'args': (16, 16)
-    },
-}
 CELERY_ALWAYS_EAGER = False
 
 
+
+
 # Because of https://github.com/GeoNode/geonode/issues/3520 we need to manually add our templates/static folders so that they can override static files/templatest
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 for app in reversed(SPCNODE_APPS):
     tpl_dir = os.path.join(BASE_DIR, app,'templates')
     stc_dir = os.path.join(BASE_DIR, app,'static')
@@ -47,27 +43,7 @@ for app in reversed(SPCNODE_APPS):
 
 ROOT_URLCONF = os.getenv('ROOT_URLCONF', 'spcnode.urls')
 
-# OFFLINE OSM
-# MAP_BASELAYERS.append(
-#     {
-#         "source": {"ptype": "gxp_olsource"},
-#         "type":"OpenLayers.Layer.WMS",
-#         "name": "Offline OSM",
-#         "group":"background",
-#         "visibility": False,
-#         "fixed": True,
-#         "args":[
-#             "Offline OSM",
-#             GEOSERVER_PUBLIC_LOCATION+"wms",
-#             {
-#                 "layers":["geonode:offline_osm_multipolygons"],
-#                 "format":"image/png",
-#                 "bgcolor":"0xb5d0d0",
-#                 "tiled": True
-#             }
-#             ]
-#     }
-# )
+
 # GOOGLE SATELLITE
 MAP_BASELAYERS.append(
     {
@@ -81,51 +57,3 @@ MAP_BASELAYERS.append(
         "fixed": True,
     }
 )
-
-# DEFAULT_MAP_BASELAYERS = [
-#     "OSM_Offline",
-#     "mapnik",
-#     "No background"
-# ]
-# DEFAULT_MAP_CENTER = (171.3526868,7.104533) # Majuro
-# DEFAULT_MAP_ZOOM = '11'
-
-
-# TEMPLATES["DIRS"] += ['../templates']
-
-
-#
-# General Django development settings
-#
-
-# SITENAME = 'spcnode'
-
-# LOCAL_ROOT = os.path.abspath(os.path.dirname(__file__))
-
-# WSGI_APPLICATION = "spcnode.wsgi.application"
-
-# DEBUG = True
-
-
-# # Load more settings from a file called local_settings.py if it exists
-# try:
-#     from local_settings import *
-# except ImportError:
-#     pass
-
-# # Additional directories which hold static files
-# STATICFILES_DIRS.append(
-#     os.path.join(LOCAL_ROOT, "static"),
-# )
-
-# # Location of url mappings
-# ROOT_URLCONF = 'spcnode.urls'
-
-# # Location of locale files
-# LOCALE_PATHS = (
-#     os.path.join(LOCAL_ROOT, 'locale'),
-#     ) + LOCALE_PATHS
-
-# INSTALLED_APPS = INSTALLED_APPS + ('spcnode',)
-
-# TEMPLATES[0]['DIRS'].insert(0, os.path.join(LOCAL_ROOT, "templates"))
