21d3b666748c16b3a32489e95a8fdca119ec2c64
Olivier Dalang <olivier.dalang@gmail.com>
2018-01-12 15:37:41 +1300

    use geoserver build geodatadir instead of starting from scratch
    authentication seems to work now !!

--- geoserver/docker-entrypoint.sh
+++ geoserver/docker-entrypoint.sh
@@ -11,7 +11,20 @@ if test "$(ls -A "/spcgeonode-geodatadir/")"; then
     printf 'Geodatadir not empty, skipping initialization...\n'
 else
     printf 'Geodatadir empty, we copy the files...\n'
-    cp -R /geodatadir-template/* /spcgeonode-geodatadir/
+
+    # We unzip the folder
+    unzip /data-2.12.x.zip -d /spcgeonode-geodatadir/
+    mv /spcgeonode-geodatadir/data/* /spcgeonode-geodatadir/
+    rm -rf /spcgeonode-geodatadir/data
+
+    # We recursively replace localhost:8000 to our django public ip
+    # We replace http://localhost:8000/geoserver with GEOSERVER_PUBLIC_URL and http://localhost:8000/ with GEONODE_PUBLIC_URL
+    sed -i 's,http://localhost:8000/geoserver,/geoserver,g' '/spcgeonode-geodatadir/security/filter/geonode-oauth2/config.xml' # TODO : parametrize GEOSERVER_PUBLIC_URL
+    sed -i 's,http://localhost:8000/,/,g' '/spcgeonode-geodatadir/security/filter/geonode-oauth2/config.xml' # TODO : parametrize GEONODE_PUBLIC_URL
+    sed -i 's,http://localhost:8000/geoserver,/geoserver,g' '/spcgeonode-geodatadir/security/role/geonode REST role service/config.xml' # TODO : parametrize GEOSERVER_PUBLIC_URL
+    sed -i 's,http://localhost:8000/,/,g' '/spcgeonode-geodatadir/security/role/geonode REST role service/config.xml' # TODO : parametrize GEONODE_PUBLIC_URL
+
+    
 fi
 
 printf '\n--- END Geoserver Docker Entrypoint ---\n\n'
