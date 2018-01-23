2885dfeed0b2c731725bd558d12ad8c64dc90a99
Olivier Dalang <olivier.dalang@gmail.com>
2018-01-24 11:48:34 +1200

    (cosmetic)

--- letsencrypt/docker-entrypoint.sh
+++ letsencrypt/docker-entrypoint.sh
@@ -54,8 +54,7 @@ else
 fi
 
 
-printf "\nWaiting a little bit\n"
-# We wait a little bit to avoid hitting Letsencrypt rate limits in case there's a failure and the the container is restarted with low delay
+printf "\nWaiting 20s to avoid hitting Letsencrypt rate limits if restarting in case of failure\n"
 sleep 20
 
 printf "\nGetting the certificates\n"
@@ -64,7 +63,12 @@ certbot --config-dir /spcgeonode-certbot/ -vvv certonly --webroot -w /spcgeonode
 printf "\nTesting autorenew\n"
 certbot --config-dir /spcgeonode-certbot/ -vvv renew --dry-run
 
+
+
+# Cron jobs
+
 printf "\n\nInstalling cronjobs\n"
+
 # notes : first one is letsencrypt (we run it twice a day), second one is autoissued (we renew every year, as it's duration is 365 days + 30 days)
 ( echo "0 0,12 * * * date && certbot renew" ; echo "0 0 1 1 * date && openssl req -x509 -nodes -days 395 -newkey rsa:2048 -keyout /spcgeonode-certbot/autoissued/$LAN_HOST/privkey.pem -out /spcgeonode-certbot/autoissued/$LAN_HOST/fullchain.pem -subj \"/CN=$LAN_HOST\"") | /usr/bin/crontab -
 # We print the crontab just for debugging purposes
--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -3,12 +3,14 @@
 # Exit script in case of error
 set -e
 
-printf "\n\nLoading nginx autoreloader\n"
-sh /docker-autoreload.sh &
-
 printf "\n\nReplacing environement variables"
 envsubst '\$LAN_HOST \$WAN_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+printf "\n\nLoading nginx autoreloader\n"
+sh /docker-autoreload.sh &
+
+
+printf "\n\nPrinting configuration for debug purposes\n"
 cat /etc/nginx/nginx.conf
 
 # Run the CMD 
