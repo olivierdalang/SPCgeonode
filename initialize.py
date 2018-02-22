117064e6d2b4b685bb24c51180146a16bd996dfd
Olivier Dalang <olivier.dalang@gmail.com>
2018-02-22 12:11:19 +1200

    django initialization as a python script

---
+++ initialize.py
@@ -0,0 +1,77 @@
+"""
+This script initializes Geonode
+"""
+
+#########################################################
+# Setting up the  context
+#########################################################
+
+import os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spcgeonode.settings')
+django.setup()
+
+
+#########################################################
+# Imports
+#########################################################
+
+from django.core.management import call_command
+from geonode.people.models import Profile
+from oauth2_provider.models import Application
+
+
+#########################################################
+# 1. Running the migrations
+#########################################################
+
+print("-"*80 + "\n1. Running the migrations")
+call_command('migrate', '--noinput')
+
+
+#########################################################
+# 2. Collecting static files
+#########################################################
+
+print("-"*80 + "\n2. Collecting static files")
+call_command('collectstatic', '--noinput')
+
+
+#########################################################
+# 3. Creating superuser if it doesn't exist
+#########################################################
+
+print("-"*80 + "\n3. Creating superuser if it doesn't exist")
+try:
+    superuser = Profile.objects.create_superuser(
+        open('/run/secrets/admin_username','r').read(),
+        os.getenv('ADMIN_EMAIL'),
+        open('/run/secrets/admin_password','r').read()
+    )
+    print('superuser successfully created')    
+except django.db.IntegrityError as e:
+    print('superuser exists already')
+
+
+#########################################################
+# 4. Create an OAuth2 provider to use authorisations keys
+#########################################################
+
+print("-"*80 + "\n4. Create an OAuth2 provider to use authorisations keys")
+try:
+    app = Application.objects.create(
+        pk=1,
+        name='GeoServer',
+        client_type='confidential',
+        authorization_grant_type='authorization-code'
+    )
+    print('oauth2 provider successfully created')
+except django.db.IntegrityError as e:
+    print('oauth2 provider exists already')
+
+
+#########################################################
+# 5. Loading fixtures
+#########################################################
+
+print("-"*80 + "\n5. Loading fixtures")
+call_command('loaddata', 'initial_data')
