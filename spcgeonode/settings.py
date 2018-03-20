f1a80b463dbf55f28c6cb81b2e377710ae0a5118
Olivier Dalang <olivier.dalang@gmail.com>
2018-03-21 10:52:29 +1300

    customization for tonga

--- spcgeonode/settings.py
+++ spcgeonode/settings.py
@@ -19,8 +19,18 @@ OGC_SERVER['default']['PASSWORD'] = open('/run/secrets/admin_password','r').read
 OGC_SERVER['default']['GEOFENCE_SECURITY_ENABLED'] = True
 
 ##################################
+# Customization for Tonga
+##################################
+
+# Install
+ADDITIONNAL_INSTALLED_APPS = ('tonga',)
+
+##################################
 # Misc / debug / hack
 ##################################
+
+# Add our additionnal installed apps
+INSTALLED_APPS = INSTALLED_APPS + ADDITIONNAL_INSTALLED_APPS
 
 # Can be removed after geonode>=2.7.x as it will be like this in main settings
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
@@ -47,3 +57,13 @@ PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.CryptPasswordHasher',
     'spcgeonode.hashers.GeoserverPlainPasswordHasher',
 )
+
+# Because of https://github.com/GeoNode/geonode/issues/3520 we need to manually add our templates/static folders so that they can override static files/templatest
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+for app in reversed(ADDITIONNAL_INSTALLED_APPS):
+    tpl_dir = os.path.join(BASE_DIR, app,'templates')
+    stc_dir = os.path.join(BASE_DIR, app,'static')
+    if os.path.exists(tpl_dir):
+        TEMPLATES[0]["DIRS"] = [tpl_dir] + TEMPLATES[0]["DIRS"]
+    if os.path.exists(stc_dir):
+        STATICFILES_DIRS = [stc_dir] + STATICFILES_DIRS
