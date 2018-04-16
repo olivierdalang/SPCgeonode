12aeb75d3fd5edaa5fa5dd4624a0bb85dcded202
Olivier Dalang <olivierd@spc.int>
2018-04-17 10:12:04 +1200

    fix rclone

--- _service_rclone/sync.sh
+++ _service_rclone/sync.sh
@@ -3,8 +3,8 @@
 # Exit script in case of error
 set -e
 
-rclone sync -v --config /run/secrets/rclone.backup.conf /spcgeonode-geodatadir/ spcgeonode:/geodatadir/
-rclone sync -v --config /run/secrets/rclone.backup.conf /spcgeonode-media/ spcgeonode:/media/
-rclone sync -v --config /run/secrets/rclone.backup.conf /spcgeonode-pgdumps/ spcgeonode:/pgdumps/
+rclone sync -v --config /run/secrets/rclone_backup_conf /spcgeonode-geodatadir/ spcgeonode:geodatadir/
+rclone sync -v --config /run/secrets/rclone_backup_conf /spcgeonode-media/ spcgeonode:media/
+rclone sync -v --config /run/secrets/rclone_backup_conf /spcgeonode-pgdumps/ spcgeonode:pgdumps/
 
 printf "\nSync finished...\n"
