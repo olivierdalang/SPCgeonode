f17e483749852ab74372a0c8fc3081108603da1f
Olivier Dalang <olivierd@spc.int>
2018-04-16 15:05:34 +1200

    fix geoserver public location typo

--- spcgeonode/settings.py
+++ spcgeonode/settings.py
@@ -41,7 +41,7 @@ else:
 
 # Manually replace SITEURL whereever it is used in geonode's settings.py
 # OGC_SERVER['default']['LOCATION'] = 'http://nginx/geoserver/' # this is already set as ENV var in the dockerfile
-OGC_SERVER['default']['PUBLIC_LOCATION'] = SITEURL + '/geoserver/'
+OGC_SERVER['default']['PUBLIC_LOCATION'] = SITEURL + 'geoserver/'
 CATALOGUE['default']['URL'] = '%scatalogue/csw' % SITEURL
 PYCSW['CONFIGURATION']['metadata:main']['provider_url'] = SITEURL
 
